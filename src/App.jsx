a9627d5ec99569ad549b3d3aa6672f327dc14697
Atif Hussain <atifhussainh@gmail.com>
2024-09-11 19:22:09 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Example from './Components/Form'
+import Examplee from './Components/Header'
+import KitchenItems from './Components/KitchenItems'
+
+const App = () => {
+  return (
+    <div> 
+    
+    
+    <Examplee/>
+    <Example/>
+    <KitchenItems/>
+      
+  
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/CardSection.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const CardSection = () => {
+  return (
+    <div>
+        <div className='w-[800px] text-center grid grid-cols-2 gap-10 mx-auto p-30 mt-10'>
+      <div className="shadow-lg">
+       <div className="overflow-hidden">
+       <img
+        src='https://t3.ftcdn.net/jpg/00/83/92/82/360_F_83928200_mYGZqB0ozTtSS6J5EtW9834mjb5tLW6x.jpg'
+        alt=''
+        className='hover:scale-125 duration-1000 w-[500px]'
+       />
+      
+
+       </div>
+       <h3>Our Building</h3>
+       <p className='text-[10px] py-2'>
+        This is a beautiful building located in the heart of London. It has been here for over 150 years.
+       </p>
+
+      </div>
+      <div className="shadow-lg">
+       <div className="overflow-hidden">
+       <img
+        src='https://t3.ftcdn.net/jpg/00/83/92/82/360_F_83928200_mYGZqB0ozTtSS6J5EtW9834mjb5tLW6x.jpg'
+        alt=''
+        className='hover:scale-125 duration-1000'
+       />
+      
+
+       </div>
+       <h3>Our Building</h3>
+       <p className='text-[10px] py-2'>
+        This is a beautiful building located in the heart of London. It has been here for over 150 years.
+       </p>
+
+      </div>
+      <div className="shadow-lg">
+       <div className="overflow-hidden">
+       <img
+        src='https://t3.ftcdn.net/jpg/00/83/92/82/360_F_83928200_mYGZqB0ozTtSS6J5EtW9834mjb5tLW6x.jpg'
+        alt=''
+        className='hover:scale-125 duration-1000'
+       />
+      
+
+       </div>
+       <h3>Our Building</h3>
+       <p className='text-[10px] py-2'>
+        This is a beautiful building located in the heart of London. It has been here for over 150 years.
+       </p>
+
+      </div>
+      <div className="shadow-lg">
+       <div className="overflow-hidden">
+       <img
+        src='https://t3.ftcdn.net/jpg/00/83/92/82/360_F_83928200_mYGZqB0ozTtSS6J5EtW9834mjb5tLW6x.jpg'
+        alt=''
+        className='hover:scale-125 duration-1000'
+       />
+      
+
+       </div>
+       <h3>Our Building</h3>
+       <p className='text-[10px] py-2'>
+        This is a beautiful building located in the heart of London. It has been here for over 150 years.
+       </p>
+
+      </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default CardSection
